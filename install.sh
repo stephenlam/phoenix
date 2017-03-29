6ade0b668e4164fbe54ab7265c1fe476026af672
Stephen Lam <stephenzlam@gmail.com>
2017-03-29 20:41:15 +0000

    Add sudo to install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ PHOENIX_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 # Create symlinks
-ln -sfv "$PHOENIX_DIR/configuration.nix" /etc/nixos/configuration.nix
+sudo ln -sfv "$PHOENIX_DIR/configuration.nix" /etc/nixos/configuration.nix
 
 # Rebuild and switch
-nixos-rebuild switch
+sudo nixos-rebuild switch
 
 
